c271e589de3fbe06554aad5448fd7fd49c55bdbd
Ross McKay <rmckay@webaware.com.au>
2021-09-20 19:27:39 +1000

    support label-based folders

--- upd-s3-folders.php
+++ upd-s3-folders.php
@@ -3,7 +3,7 @@
 Plugin Name: UDP S3 Folders
 Update URI: udp-s3-folders
 Description: add Amazon S3 path folders for different backups on UpdraftPlus
-Version: 0.0.1
+Version: 0.0.2
 Author: WebAware
 Author URI: https://shop.webaware.com.au/
 */
@@ -26,6 +26,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+namespace webawareau\udp_s3_folders;
+
+use UpdraftPlus_BackupModule_s3;
+
 if (!defined('ABSPATH')) {
 	exit;
 }
@@ -38,8 +42,51 @@ if (!defined('ABSPATH')) {
  */
 add_filter('updraftplus_backupmodule_get_options', function($options, $module) {
 	if ($module instanceof UpdraftPlus_BackupModule_s3) {
+		// set folder to weekly if it's Sunday, else daily
 		$folder = 0 === (int) date('w') ? 'weekly' : 'daily';
+
+		// if running from wp-cli, check for specific backup labels which require their own folder
+		if (is_wpcli()) {
+			$label = get_wpcli_label();
+
+			switch ($label) {
+
+				case 'db':
+				case 'longdb':
+					$folder = $label;
+					break;
+
+			}
+		}
+
 		$options['path'] = rtrim($options['path'], '/') . "/$folder";
 	}
 	return $options;
 }, 10, 2);
+
+/**
+ * get label option from wp-cli
+ * @return string
+ */
+function get_wpcli_label() {
+	global $argv;
+
+	$label = '';
+
+	foreach ($argv as $arg) {
+		$arg = explode('=', $arg, 2);
+		if (count($arg) === 2 && $arg[0] === '--label') {
+			$label = $arg[1];
+		}
+	}
+
+	return $label;
+}
+
+/**
+ * check to see if running from wp-cli
+ * @return bool
+ */
+function is_wpcli() {
+	return defined('WP_CLI') && WP_CLI;
+}
